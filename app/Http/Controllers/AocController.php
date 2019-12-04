efa3355077365e8f0997d22e4701ee968e667739
Kyle Boehlen <kboehlen@kengarff.com>
2019-12-04 13:34:59 -0700

    API scaffolding and validation

--- app/Http/Controllers/AocController.php
+++ app/Http/Controllers/AocController.php
@@ -2,20 +2,98 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 class AocController extends Controller
 {
+    private $rules = [
+        'year' => 'required|integer|between:2017,2019',
+        'day' =>  'required|integer|between:1,25',
+        'part' => 'required|integer|between:1,2', 
+        'input' => 'required',
+    ];
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Request $request)
     {
-        //
+        // Set param array
+        $this->params = [
+            'year' => $request->route('year'),
+            'day' => $request->route('day'),
+            'part' => $request->route('part'),
+        ];
+
+        if($request->has('input'))
+        {
+            $this->params['input'] = $request->get('input');
+        }
     }
 
-    public function index($year, $day, $part)
+    public function index()
     {
-        return $year . '-' . $day . '-' . $part;
+        // Verify params
+        $validator = Validator::make($this->params, $this->rules);
+
+        if($validator->fails())
+        {
+            return $this->buildResponse($validator->errors(), 400);
+        }
+
+        // Set vars from params array
+        foreach($this->params as $key => $value)
+        {
+            $$key = $value;
+        }
+
+        // Find function by year and day
+        switch($year)
+        {
+            case 2017:
+                switch($day)
+                {
+                    default:
+                        return $this->buildResponse("Day $day is not available for $year", 404);
+                    break;
+                }
+                break;
+            case 2018:
+                switch($day)
+                {
+                    default:
+                        return $this->buildResponse("Day $day is not available for $year", 404);
+                    break;
+                }
+                break;
+            case 2019:
+                switch($day)
+                {
+                    default:
+                        return $this->buildResponse("Day $day is not available for $year", 404);
+                    break;
+                }
+                break;
+            default:
+                return $this->buildResponse("Year $year is not available", 404);
+                break;
+        }
+    }
+
+    // Function to build response based on output
+    private function buildResponse($output, $status = 200)
+    {
+        // Serialize array responses to json
+        if(is_array($output))
+        {
+            return response()->json($output, $status);
+        }
+        else
+        {
+            return response($output, $status);
+        }
     }
 }
